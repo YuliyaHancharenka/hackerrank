94b2ff893c8eb647ac71d5695a1113bd536d821c
yuliya.hancharenka <Creator13>
2021-12-19 22:52:44 +0530

    Add JavaBigDecimal

--- src/hackerrank/java/bigNumber/JavaBigDecimal.java
+++ src/hackerrank/java/bigNumber/JavaBigDecimal.java
@@ -2,7 +2,7 @@ package hackerrank.java.bigNumber;
 
 import java.math.BigDecimal;
 import java.util.Scanner;
-//TODO
+
 public class JavaBigDecimal {
     public static void main(String[] args) {
         //Input
@@ -15,17 +15,19 @@ public class JavaBigDecimal {
         sc.close();
 
         //Sorting (Selection)
-        for (int i = 0; i < (s.length - 2); i++) {
-            for (int k = (i + 1); k < s.length - 2; k++) {
-                if (new BigDecimal(s[i]).compareTo(new BigDecimal(s[k])) < 0) {
-                    String tempValue = s[i];
-                    s[i] = s[k];
-                    s[k] = tempValue;
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j >= 1; j--) {
+                if (new BigDecimal(s[j]).compareTo(new BigDecimal(s[j - 1])) > 0) {
+                    String temp = s[j];
+                    s[j] = s[j - 1];
+                    s[j - 1] = temp;
+                } else {
+                    break;
                 }
             }
         }
-        //Output
 
+        //Output
         for (int i = 0; i < n; i++) {
             System.out.println(s[i]);
         }
