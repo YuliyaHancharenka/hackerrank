cf0a0667681134c19162cbdb96b453d99f2c4622
yuliya.hancharenka <Creator13>
2021-12-19 21:48:21 +0530

    Add DuplicateWords

--- src/hackerrank/java/strings/DuplicateWords.java
+++ src/hackerrank/java/strings/DuplicateWords.java
@@ -3,25 +3,28 @@ package hackerrank.java.strings;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-//TODO
+
+
 public class DuplicateWords {
     public static void main(String[] args) {
-        String pattern = "\\b(\\w+)(\\b\\W+\\b\\1\\b)*";
-        Pattern r = Pattern.compile(pattern, Pattern.MULTILINE + Pattern.CASE_INSENSITIVE);
+        String regex = "\\b(\\w+)(\\s+\\1\\b)+";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 
         Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine());
+        int numSentences = Integer.parseInt(in.nextLine());
 
-        while (testCases > 0) {
+        while (numSentences-- > 0) {
             String input = in.nextLine();
-            Matcher m = r.matcher(input);
-            boolean findMatch = true;
+
+            Matcher m = p.matcher(input);
+
+            // Check for subsequences of input that match the compiled pattern
             while (m.find()) {
                 input = input.replaceAll(m.group(), m.group(1));
-                findMatch = false;
             }
+            // Prints the modified sentence.
             System.out.println(input);
-            testCases--;
         }
+        in.close();
     }
 }
