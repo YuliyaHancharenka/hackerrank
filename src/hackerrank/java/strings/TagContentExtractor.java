c3e77912ed19e27158fb66f492af516855b9176e
yuliya.hancharenka <Creator13>
2021-12-19 21:57:17 +0530

    Add TagContentExtractor

--- src/hackerrank/java/strings/TagContentExtractor.java
+++ src/hackerrank/java/strings/TagContentExtractor.java
@@ -1,18 +1,30 @@
 package hackerrank.java.strings;
 
 import java.util.Scanner;
-//TODO
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TagContentExtractor {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine());
-        while(testCases>0){
+        int t = Integer.parseInt(in.nextLine());
+
+        while (t-- > 0) {
             String line = in.nextLine();
 
-            //Write your code here
+            Matcher m = Pattern.compile("<(.+)>(([^<>]+))</\\1>").matcher(line);
 
-            testCases--;
+            if (!m.find()) {
+                System.out.println("None");
+                continue;
+            }
+
+            m.reset();
+            while (m.find()) {
+                System.out.println(m.group(2));
+            }
         }
     }
 }
+
