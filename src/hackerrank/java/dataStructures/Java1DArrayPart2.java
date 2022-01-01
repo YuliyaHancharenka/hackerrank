610410b2bd4d330053e1a449e1e2dd15b211279a
yuliya.hancharenka <Creator13>
2022-01-01 19:23:27 +0530

    Add Java1DArrayPart2.java

--- src/hackerrank/java/dataStructures/Java1DArrayPart2.java
+++ src/hackerrank/java/dataStructures/Java1DArrayPart2.java
@@ -1,7 +1,24 @@
 package hackerrank.java.dataStructures;
 
 
+import java.util.Scanner;
+
 public class Java1DArrayPart2 {
 
+    public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for(int i = 0 ; i < n; i++){
+            int val = scan.nextInt();
+            a[i] = val;
+        }
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int value : a) {
+            System.out.println(value);
+        }
+    }
 }
